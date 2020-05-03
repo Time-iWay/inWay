aefee8380cc116c204c86ac4fd0c63bc325ef830
Victor Lima <victormamed2016@gmail.com>
2020-05-03 02:13:13 -0300

    Desenvolvendo a Home/incompleta]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="header">
+           <div className="logo">
+                <img src={logo} alt="InWay"/>
+            </div>
+            <div className="menu">
+                <ul className="menu-content">
+                    <li className="menu-item">Já sou <span>Investidor</span></li>
+                    <li className="menu-item">Já sou <span>Consultor</span></li>
+                </ul>
+            </div>
+        </header>
+        
+        <div className="body">
+            <div className="textos">
+                <div className="body-texto">
+                    <h1 className="title">Incentivando quem <span>quer</span>, Indicando quem <span>pode.</span></h1>
+                </div>  
+
+                <div className="body-sub">
+                    <h5 className="subtitle">Gostaria-mos de lhe chamar pelo nome! Preencha os espaços e selecione uma das opções para lhe conhecer-mos melhor.</h5>
+                </div>  
+            </div>
+            <div className="form">  
+                <div className="body-form">
+                    <input className="input" type="text" placeholder="Nome" autoComplete="off" required/>
+                    <input className="input" type="email" placeholder="Email" autoComplete="off" required/>
+
+                    <div className="btns">
+                        <button className="btn">Investidor</button>
+                        <h5 className="ou">- ou -</h5>
+                        <button className="btn">Consultor</button>
+                    </div>    
+                </div>                     
+            </div>
+        </div>
     </div>
   );
 }
