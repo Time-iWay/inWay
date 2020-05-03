369bd441a0a0f3f0f6d028ff0d33b87303f4ed14
Eduardo Barbosa Weschenfelder <43359067+EduardoWeschenfelder@users.noreply.github.com>
2020-05-03 10:20:47 -0300

    Ajustes para rotas

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Cadastro from "./pages/cadastroColsult/index";
-
-const App = () => {
-  return <Cadastro />;
-};
-
-export default App;
---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Consult from "./pages/cadastroColsult/index";
+import Home from "./pages/home/app";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/new/consult" component={Consult} />
+        {/* <Route path="/" component={} /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+}
