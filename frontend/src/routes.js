7428c2ddbef0908855d5c2f94031f018ee6c3c7e
Victor Lima <victormamed2016@gmail.com>
2020-05-03 22:39:12 -0300

    Add imagens

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Consult from "./pages/cadastroColsult/index";
-import Home from "./pages/home/App";
+import Home from "./pages/home/app";
 
 export default function Routes() {
   return (
